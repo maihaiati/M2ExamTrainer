424c86019008fba1f653ae791aa1f62ceb986437
M2Tech <maiquanghai011@gmail.com>
2024-09-30 14:54:35 +0700

    Create C# version

---
+++ CSharpVersion/M2ExamTrainer/M2ExamTrainer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace M2ExamTrainer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
