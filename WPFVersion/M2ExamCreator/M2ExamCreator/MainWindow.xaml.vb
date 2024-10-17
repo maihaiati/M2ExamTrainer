a1b6d73a56645d1ec28262ef9647c1b429a40fa2
M2Tech <maiquanghai011@gmail.com>
2024-10-17 20:22:42 +0700

    Convert C# to VB

--- WPFVersion/M2ExamCreator/M2ExamCreator/MainWindow.xaml.vb
+++ WPFVersion/M2ExamCreator/M2ExamCreator/MainWindow.xaml.vb
@@ -1,3 +1,8 @@
 ﻿Class MainWindow
+	Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+		Dim startup As New Startup
 
+		MainGrid.Children.Clear()
+		MainGrid.Children.Add(startup)
+	End Sub
 End Class
--- WPFVersion/M2ExamCreator/M2ExamCreator/Startup.xaml.vb
+++ WPFVersion/M2ExamCreator/M2ExamCreator/Startup.xaml.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Startup
+	Private Sub btnCreate_Click(sender As Object, e As RoutedEventArgs)
+		Dim mainWindow As MainWindow = CType(Application.Current.MainWindow, MainWindow)
+		Dim createExam As New CreateExam
 
+		mainWindow.MainGrid.Children.Clear()
+		mainWindow.MainGrid.Children.Add(createExam)
+	End Sub
+
+	Private Sub btnBrowse_Click(sender As Object, e As RoutedEventArgs) Handles btnBrowse.Click
+		Dim examFile = QuickAction.GetFile()
+
+		If Not String.IsNullOrWhiteSpace(examFile) And examFile <> "CANCEL_SELECT_FILE" Then
+			Dim mainWindow As MainWindow = CType(Application.Current.MainWindow, MainWindow)
+			Dim editExam As EditExam = New EditExam(examFile)
+
+			mainWindow.MainGrid.Children.Clear()
+			mainWindow.MainGrid.Children.Add(editExam)
+		End If
+	End Sub
 End Class
